c3328ac0fced20a08a9410807e33505d7534e4f9
sedamcheon <sedamcheon@gmail.com>
2026-06-11 20:31:27 +0900

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,7 +0,0 @@
-window.CAFE1_FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "ccff-58ba5.firebaseapp.com",
-  databaseURL: "https://ccff-58ba5-default-rtdb.firebaseio.com",
-  projectId: "ccff-58ba5",
-  appId: ""
-};
